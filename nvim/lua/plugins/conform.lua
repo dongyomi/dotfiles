5b6a8d16e927ae81f9021a64f05f1e7c53a14deb
dym <dhti375322@naver.com>
2026-02-17 07:00:29 +0900

    feat: add json formatter in nvim

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,6 +4,7 @@ return {
         formatters_by_ft = {
             lua = { "stylua" },
             c = { "clang-format" },
+            json = { "jq" },
         },
         format_on_save = {
             timeout_ms = 500,
