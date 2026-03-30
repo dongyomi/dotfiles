a88548f02fbb41884c06b60d02f9c29ff962427b
dym <dhti375322@naver.com>
2026-03-31 01:19:19 +0900

    fix: change nvim installation method

--- install.sh
+++ install.sh
@@ -9,7 +9,10 @@ rm -rf ~/.config/kitty ~/.config/nvim
 sudo apt update
 sudo apt install -y kitty curl xclip fd-find ripgrep python3.12-venv postgresql tree lua5.1 r-base libpq-dev libcurl4-openssl-dev libgit2-dev libssl-dev libssh2-1-dev libxml2-dev libfontconfig1-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev libharfbuzz-dev libfribidi-dev redis
 
-sudo snap install nvim --classic
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+chmod +x nvim-linux-x86_64.appimage
+sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
+
 sudo snap install dbeaver-ce --classic
 
 ln -sf $HOME/dotfiles/kitty ~/.config/kitty 
